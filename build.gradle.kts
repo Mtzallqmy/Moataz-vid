cf7ff82524011014c0fda948689947b525dd23a5
Mtzallqmy <mtzallqmy@gmail.com>
2026-09-01 06:01:27 +0300

    build(stage10): prepare production app version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.jvm) apply false
@@ -9,5 +10,5 @@ plugins {
 
 allprojects {
     group = "com.moatazvid"
-    version = "0.1.0"
+    version = "1.0.0"
 }
